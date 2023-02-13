cf69420eec66cd392635c5ca02769a8dd3ada46e
SergioDegas <laferces@gmail.com>
2023-02-13 21:20:09 +0200

    345

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { StyledEngineProvider } from '@mui/material/styles';
 import './index.css';
-import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider > */}
-      <StyledEngineProvider injectFirst>
-        <App />
-      </StyledEngineProvider>
-    {/* </Provider> */}ыававававававававававававававававпро
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
