9b6e16045dceee122d73d6d54a4079a60e184fbf
SergioDegas <laferces@gmail.com>
2023-02-13 21:17:26 +0200

    нг

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,38 @@
 
 
 
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { ContactAddApi } from './Api';
 import {ResponsiveBar} from './AppBar/AppBar'
 
 
 export const App = () => {
 
+  // dispatch(() => ContactAddApi());
+// ContactAddApi()
+ 
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const text = form.elements.text.value;
+    console.log(text);
+    if (text !== '') {
+      // dispatch(ContactAddApi(text));
+      form.reset();
+      return;
+    }
+    alert('Task cannot be empty. Enter some text!');
+  };
    return (
-    <>
-      <ResponsiveBar />
-  
-    </>
-  );
+     <>
+       <ResponsiveBar />
+       <form  onSubmit={handleSubmit}>
+         <input name="text"  />
+         <button type="submit" >
+           Add task
+         </button>
+       </form>
+     </>
+   );
 };
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { StyledEngineProvider } from '@mui/material/styles';
 import './index.css';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-    <App />
-    </StyledEngineProvider>
+    {/* <Provider > */}
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
+    {/* </Provider> */}ыававававававававававававававававпро
   </React.StrictMode>
 );
