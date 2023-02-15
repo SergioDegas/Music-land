ba9c3ae47ca2e962d344d4db768b485f7a8cc031
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2023-02-15 19:48:46 +0200

    edits

---
+++ src/components/AppBar/Register.jsx
@@ -0,0 +1,22 @@
+import Button from '@mui/material/Button';
+
+export const Register = () => {
+return(
+    <>
+    <Button
+              sx={{ position: 'fixed', my: 2, color: 'white', right: 300 }}
+            >
+              Register
+            </Button>
+            <Button
+              sx={{ position: 'fixed', my: 2, color: 'white', right: 200 }}
+            >
+              Log In
+            </Button>
+    </>
+    
+    )
+
+
+
+}
