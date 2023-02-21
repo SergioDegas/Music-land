a0773986b1501fbf99a28bd952ae60fcec660b31
SergioDegas <laferces@gmail.com>
2023-02-21 20:13:18 +0200

    dfg

--- src/components/Cards/CardsByID/CardsByID.jsx
+++ src/components/Cards/CardsByID/CardsByID.jsx
@@ -1,7 +1,7 @@
 import {   Box } from '@mui/material';
 import { selectMovieID } from 'components/redux/recipes/selector';
 import { useEffect } from 'react';
-import { Absolute, BackdropPath, Relative, Backdrop } from './CardsByID.styled';
+import { Absolute, BackdropPath, Relative, Backdrop, PosterPath, Title, Genres, Text, TextTitle } from './CardsByID.styled';
 
 const { getMovieDetails } = require('components/redux/recipes/operation');
 const { useDispatch, useSelector } = require('react-redux');
@@ -39,7 +39,7 @@ function CardsByID() {
             <div key={id}>
               <Relative>
                 <Box>
-                  <Backdrop></Backdrop>
+                  <Backdrop />
                   <BackdropPath
                     src={
                       backdrop_path
@@ -50,22 +50,36 @@ function CardsByID() {
                   />
                 </Box>
                 <Absolute>
-                  <div>
-                    <img
-                      src={
-                        poster_path
-                          ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-                          : 'Sorry, there is no picture here '
-                      }
-                      alt={original_title}
-                    />
-                    <h2> {original_title}</h2>
-                    {genres &&
-                      genres.map(({ name, id }) => <p key={id}>{name}</p>)}
-                    <p>{overview}</p>
-                    <p>{release_date}</p>
-                    <p>{vote_average}</p>
-                  </div>
+                  <Box sx={{ display: 'flex' }}>
+                    <Box>
+                      <PosterPath
+                        src={
+                          poster_path
+                            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+                            : 'Sorry, there is no picture here '
+                        }
+                        alt={original_title}
+                      />
+                    </Box>
+
+                    <Box sx={{ ml: 4 }}>
+                      <Box sx={{ display: 'flex' }}>
+                        <Title>{original_title}</Title>
+                        <TextTitle>({release_date.slice(0, 4)})</TextTitle>
+                      </Box>
+
+                      <Box sx={{ display: 'flex', gap: '15px' }}>
+                        <Text>{release_date}</Text>
+                        {genres &&
+                          genres.map(({ name, id }) => (
+                            <Text key={id}>{name}</Text>
+                          ))}
+                      </Box>
+                      <Text>{overview}</Text>
+
+                      <Text>{vote_average}</Text>
+                    </Box>
+                  </Box>
                 </Absolute>
               </Relative>
             </div>
