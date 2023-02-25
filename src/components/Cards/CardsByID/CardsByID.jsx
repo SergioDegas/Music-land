6548df00a9ed428503a7a36002c9b6c92e3e9d90
SergioDegas <laferces@gmail.com>
2023-02-25 22:13:27 +0200

    card

--- src/components/Cards/CardsByID/CardsByID.jsx
+++ src/components/Cards/CardsByID/CardsByID.jsx
@@ -1,14 +1,17 @@
-import { Box } from '@mui/material';
+import { Box, Button, Modal } from '@mui/material';
+import { Container } from '@mui/material';
 import {
   getMovieDetails,
   getMovieTrailer,
 } from 'components/redux/recipes/operation';
 import { selectMovieID } from 'components/redux/recipes/selector';
 import moment from 'moment/moment';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CircularDeterminate } from '../Circular';
 import { Trailer } from '../Trailer/Trailer';
+import { BsFillPlayFill } from 'react-icons/bs';
+
 import {
   Absolute,
   BackdropPath,
@@ -18,11 +21,17 @@ import {
   Title,
   Text,
   TextTitle,
+  ModalBox,
 } from './CardsByID.styled';
 
 function CardsByID({ id }) {
+  const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
   const movieDetails = useSelector(selectMovieID);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     dispatch(getMovieDetails(id));
@@ -31,8 +40,7 @@ function CardsByID({ id }) {
   useEffect(() => {
     dispatch(getMovieTrailer(id));
   }, [dispatch, id]);
-
-  // console.log(trailerKey);
+  console.log(movieDetails);
   return (
     <>
       <section>
@@ -47,6 +55,8 @@ function CardsByID({ id }) {
               release_date,
               vote_average,
               id,
+              production_countries,
+              tagline,
             }) => (
               <div key={id}>
                 {' '}
@@ -62,47 +72,120 @@ function CardsByID({ id }) {
                       alt={original_title}
                     />
                   </Box>
+
                   <Absolute>
-                    <Box sx={{ display: 'flex' }}>
-                      <Box>
-                        <PosterPath
-                          src={
-                            poster_path
-                              ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-                              : 'Sorry, there is no picture here '
-                          }
-                          alt={original_title}
-                        />
-                      </Box>
-
-                      <Box sx={{ ml: 4 }}>
-                        <Box sx={{ display: 'flex' }}>
-                          <Title>{original_title}</Title>
-                          <TextTitle>({release_date.slice(0, 4)})</TextTitle>
+                    <Container maxWidth="lg">
+                      <Box sx={{ display: 'flex' }}>
+                        <Box>
+                          <PosterPath
+                            src={
+                              poster_path
+                                ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+                                : 'Sorry, there is no picture here '
+                            }
+                            alt={original_title}
+                          />
                         </Box>
-                        <Box sx={{ display: 'flex', gap: '15px' }}>
-                          <Text>
-                            {moment(release_date, 'YYYY-MM-DD').format(
-                              'DD.MM.YY'
-                            )}
+
+                        <Box sx={{ ml: 4 }}>
+                          <Box sx={{ display: 'flex' }}>
+                            <Title>{original_title}</Title>
+                            <TextTitle>({release_date.slice(0, 4)})</TextTitle>
+                          </Box>
+                          <Box sx={{ display: 'flex', gap: '15px' }}>
+                            <Text>
+                              {moment(release_date, 'YYYY-MM-DD').format(
+                                'DD.MM.YY'
+                              )}
+                            </Text>
+                            {production_countries &&
+                              production_countries.map(
+                                ({ iso_3166_1, name }) => (
+                                  <Text key={name}>({iso_3166_1}) </Text>
+                                )
+                              )}
+                            <Box
+                              sx={{
+                                display: 'flex',
+                                gap: '10px',
+                                flexWrap: 'wrap',
+                              }}
+                            >
+                              {genres &&
+                                genres.map(({ name, id }) => (
+                                  <Text key={id}>| {name}</Text>
+                                ))}
+                            </Box>
+                          </Box>
+                          <Box
+                            sx={{
+                              display: 'flex',
+                              flexWrap: 'nowrap',
+                              alignItems: 'center',
+                            }}
+                          >
+                            <CircularDeterminate percent={vote_average} />
+                            <Text
+                              style={{
+                                marginLeft: '10px',
+                                width: '90px',
+                                marginRight: '10px',
+                              }}
+                            >
+                              User evaluation
+                            </Text>
+                            <Button
+                              sx={{ color: 'white' }}
+                              onClick={handleOpen}
+                            >
+                              <BsFillPlayFill
+                                style={{ width: '33px', height: '33px' }}
+                              />{' '}
+                              Open Trailer
+                            </Button>
+                            <Modal
+                              open={open}
+                              onClose={handleClose}
+                              aria-labelledby="modal-modal-title"
+                              aria-describedby="modal-modal-description"
+                            >
+                              {Array.isArray(movieDetails) && (
+                                <ModalBox>
+                                  <Trailer width={1400} height={700} />
+                                </ModalBox>
+                              )}
+                            </Modal>
+                          </Box>
+                          <Text
+                            style={{
+                              marginBottom: '0',
+                              fontSize: '1.1em',
+                              fontWeight: '400',
+                              fontStyle: 'italic',
+                              opacity: '0.7',
+                            }}
+                          >
+                            {tagline}
                           </Text>
-
-                          {genres &&
-                            genres.map(({ name, id }) => (
-                              <Text key={id}>{name}</Text>
-                            ))}
+                          <Text
+                            style={{
+                              fontWeight: '600',
+                              fontSize: '1.3em',
+                              marginBottom: ' 8px',
+                            }}
+                          >
+                            {' '}
+                            Descrition
+                          </Text>
+                          <Text>{overview}</Text>
                         </Box>
-                        <Text>{overview}</Text>
-                        <CircularDeterminate percent={vote_average} />
-                        {/* <Text>{vote_average}</Text> */}
                       </Box>
-                    </Box>
+                    </Container>
                   </Absolute>
-                </Relative>{' '}
+                </Relative>
               </div>
             )
           )}
-        {Array.isArray(movieDetails) && <Trailer width={500} height={500} />}
       </section>
     </>
   );
