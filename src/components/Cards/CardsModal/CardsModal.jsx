5c11599ad8a3b28aff514fab097e3e3e1bcb5100
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2023-02-26 01:31:01 +0200

    d

--- src/components/Cards/CardsModal/CardsModal.jsx
+++ src/components/Cards/CardsModal/CardsModal.jsx
@@ -4,7 +4,9 @@ import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { getMovieDetails } from 'components/redux/recipes/operation';
+import {
+  getMovieDetails, getMovieTrailer,
+} from 'components/redux/recipes/operation';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { selectModal } from 'components/redux/recipes/selector';
@@ -19,13 +21,17 @@ import {
   GenreItem,
   GenreText,
   GenreTitle,
-  BtnLink
+  BtnLink,
 } from './CardsModalStyled';
 import IconButton from '@mui/material/IconButton';
 import moment from 'moment/moment';
-import { Backdrop }  from '../CardsByID/CardsByID.styled';
+import { Backdrop } from '../CardsByID/CardsByID.styled';
 import CloseIcon from '@mui/icons-material/Close';
 import { Link } from 'react-router-dom';
+import { Trailer } from '../Trailer/Trailer';
+import { ChildModal } from './ChildModal';
+
+
 
 const style = {
   position: 'absolute',
@@ -43,14 +49,18 @@ const BASE_BACKDROP = 'https://image.tmdb.org/t/p/original';
 const BASE_IMG = 'https://image.tmdb.org/t/p/w342';
 export const FilmModal = ({ id }) => {
   const [open, setOpen] = useState(false);
+  const [show, setShown] = useState(false);
   const movie = useSelector(selectModal);
   const dispatch = useDispatch();
+
+
   useEffect(() => {
     if (!open) {
       return;
     }
     dispatch(getMovieDetails(id));
   }, [dispatch, open, id]);
+  
   const handleOpen = () => {
     setOpen(true);
   };
@@ -58,10 +68,19 @@ export const FilmModal = ({ id }) => {
 
   return (
     <div>
-      <Link to={`/movie/${id}`} style={{textDecoration:'none'}}>
-      <Button size='small' variant='contained' sx={{p:-1,ml:0.5}}>Movie Page</Button>
+      <Link to={`/movie/${id}`} style={{ textDecoration: 'none' }}>
+        <Button size="small" variant="contained" sx={{ p: -1, ml: 0.5 }}>
+          Movie Page
+        </Button>
       </Link>
-      <Button size='small' variant='contained' sx={{p:-1,ml:0.5}} onClick={handleOpen}>Preview</Button>
+      <Button
+        size="small"
+        variant="contained"
+        sx={{ p: -1, ml: 0.5 }}
+        onClick={handleOpen}
+      >
+        Preview
+      </Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -80,11 +99,11 @@ export const FilmModal = ({ id }) => {
               genres,
               status,
               overview,
-              original_language
+              original_language,
             }) => {
               return (
                 <ModalBody key={id}>
-                  <Backdrop/>
+                  <Backdrop />
                   <BackdropPath
                     src={
                       !backdrop_path
@@ -98,12 +117,18 @@ export const FilmModal = ({ id }) => {
                     alt="img"
                   />
                   <div
-                    style={{ position: 'absolute', top: '10%', left: '40%',zIndex:3 }}
+                    style={{
+                      position: 'absolute',
+                      top: '10%',
+                      left: '40%',
+                      zIndex: 3,
+                    }}
                   >
                     <Title>{title}</Title>
-                    <Release>Release date: {moment(release_date, 'YYYY-MM-DD').format(
-                              'DD.MM.YY'
-                            )}</Release>
+                    <Release>
+                      Release date:{' '}
+                      {moment(release_date, 'YYYY-MM-DD').format('DD.MM.YY')}
+                    </Release>
                     <GenreTitle>Genres:</GenreTitle>
                     <GenresList>
                       {genres &&
@@ -111,27 +136,31 @@ export const FilmModal = ({ id }) => {
                           <GenreItem key={genre.id}>
                             <GenreText>| {genre.name}</GenreText>
                           </GenreItem>
-                       
                         ))}
                     </GenresList>
                     <Release>Status: {status} !</Release>
                     <Release>{overview}</Release>
                     <Release> Original language: {original_language}</Release>
-                    <div style={{display:'flex',marginTop:'50px'}}>
-                    <BtnLink to={`/movie/${id}`}>
-                    <Button style={{
-                    backgroundColor:'#21b6ae'
-                    }}
-                    variant="contained"
-                    >Film Card
-                    </Button>
-                    </BtnLink>
+                    <div style={{ display: 'flex', marginTop: '50px',gap:20 }}>
+                      <BtnLink to={`/movie/${id}`}>
+                        <Button
+                          style={{
+                            backgroundColor: '#21b6ae',
+                          }}
+                          variant="contained"
+                        >
+                          Film Card
+                        </Button>
+                      </BtnLink>
+                      <ChildModal id={id}/>
                     </div>
+                    
                   </div>
-                  <IconButton 
-                  sx={{position:'absolute',right:10,zIndex:3,}} 
-                  onClick={handleClose}>
-                    <CloseIcon sx={{fill:'white'}}/>
+                  <IconButton
+                    sx={{ position: 'absolute', right: 10, zIndex: 3 }}
+                    onClick={handleClose}
+                  >
+                    <CloseIcon sx={{ fill: 'white' }} />
                   </IconButton>
                 </ModalBody>
               );
