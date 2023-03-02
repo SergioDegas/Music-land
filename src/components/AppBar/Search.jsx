243c2582622d06a85c248884b3c678ca48786eac
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2023-03-02 18:51:39 +0200

    ed

--- src/components/AppBar/Search.jsx
+++ src/components/AppBar/Search.jsx
@@ -1,10 +1,23 @@
-
 import { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import { SearchBack, InputBox, Input, SearchBtn, Overlay } from './SearchStyle';
+import { searchByName } from 'components/redux/recipes/operation';
+import { selectSearch } from 'components/redux/recipes/selector';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 export const Search = () => {
   const [show, setShow] = useState(false);
+  const dispatch = useDispatch()
+  const query = useSelector(selectSearch);
+useEffect(() => {
+  if(!query){
+    return
+    }
+    dispatch(searchByName())
+}, [dispatch,query])
+
+
   const handleShow = () => {
     !show ? setShow(true) : setShow(false);
   };
@@ -18,7 +31,12 @@ export const Search = () => {
           <SearchBack>
             <form>
               <InputBox>
-                <Input type="text" placeholder="Search..." autoComplete="off" />
+                <Input 
+                type="text" 
+                placeholder="Search..." 
+                autoComplete="off" 
+
+                />
                 <SearchBtn type="submit">Search...</SearchBtn>
               </InputBox>
             </form>
